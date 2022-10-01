fecb32b3f0ec1147da31a70cf7b840117f27ea7a
Gabriel dos Santos Santana <92961406+Gabriels21@users.noreply.github.com>
2022-09-30 23:31:48 -0300

    Add files via upload

---
+++ Tipos de dados PHP/Tipos de Dados.php
@@ -0,0 +1,23 @@
+<?php 
+// Tipo de Dados
+// =============
+
+# As variáveis podem ser de diferentes tipos de dados
+# A mesma variável pode conter tipos de dados diferentes
+
+# Atribuições de tipos diferentes para a mesma variável 
+$variavel = 100;
+$variavel = "cem";
+
+# Apesar das variáveis serem de tipo implicito
+# é importante conhecer alguns tipos de variáveis
+
+$inteiro = 1000;             # Valores seguidos sem casas decimais
+$float = 100.5;              # Valores seguidos de casas decimais
+$bool = true;               # Assume valores falso ou verdadeiro
+$string = "Bom dia";        # Variável de texto com comportamento de uma cadeia de caracteres
+$array = [1, 2, 3, 4, 5];   # Variável com coleção de valores
+$nulo = null;               # Variavel com valor nulo
+// $pessoa = new Pessoa();  # Objeto com propriedade e métodos
+
+?>
