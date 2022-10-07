57c55b2ff9def34b8863baf6de1c40f06b7d1ed7
Gabriel dos Santos Santana <92961406+Gabriels21@users.noreply.github.com>
2022-10-06 23:24:21 -0300

    Add files via upload

---
+++ Operadores PHP/Operador Atribuição.sh
@@ -0,0 +1,68 @@
+<?php 
+// Operadores - O que é / Operador de atribuição
+// ==========
+# são simbolos que permitem executar operações matemáticas
+# ou operações lógicas / comparativas
+
+// Parte 1
+# Operador de atribuição utilizando o sinal de "="
+
+echo "<b>Parte 1 - Operador de atribuição utilizando o sinal de '='</b> <br><br>";
+$var = 100;
+echo "Valor atribuido: " . $var;
+
+echo "<br>";
+echo "<br>";
+// Parte 2
+
+# Alguns operadores de atribuição
+
+echo "<b>Parte 2 - Operadores Matematicos </b><br><br>";
+
+$a = 10;
+$b = 20;
+$x = $y = $z = 100;
+echo "Soma de y + z: " . $x;
+
+echo "<br>";
+echo "<br>";
+# Alguns operadores matemáticos 
+
+# Adição
+$resultado = $a + $b;
+echo "Adicao: " . $resultado;
+
+echo "<br>";
+echo "<br>";
+
+# Subtração
+$x = $b - $a;
+echo "Subtracao: " . $x;
+
+echo "<br>";
+echo "<br>";
+
+# Multiplicação
+$x = $a * $b;
+echo "Multiplicacao: " . $x;
+
+echo "<br>";
+echo "<br>";
+
+# Divisão
+$x = $b / $a;
+echo "Divisao: " .  $x;
+
+echo "<br>";
+echo "<br>";
+
+# Módulo (Resto da divisão)
+$x = 4 % 2; 
+echo "Modulo: " . $x;
+
+echo "<br>";
+echo "<br>";
+$x = 10 % 3; # Resultado 3 sobra 1 
+echo "É exibido: " . $x;
+
+?>
---
+++ Operadores PHP/Operador de Resto.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Operador de resto
+
+echo 3462/7;
+echo "<br>";
+
+echo 3462 % 7;
+// Pode ser utilizado na identificação de numeros pares e outros
+
+// Operador de potência
+$v = 5 ** 2;
+echo $v;
+
+?>
