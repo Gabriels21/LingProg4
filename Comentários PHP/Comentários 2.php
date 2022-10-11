d8e7781c0d1cbf6fe3e3018920d1166b026a06e6
Gabriel dos Santos Santana <92961406+Gabriels21@users.noreply.github.com>
2022-10-10 23:55:15 -0300

    Add files via upload

---
+++ Comentários PHP/Comentários 2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Projeto 4</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+
+    <?php echo '1º Teste 1'; ?>
+    <br>
+    <?php echo '1º Teste 2'; ?>
+    <br>
+    <?php print'2º Teste 1' ?>
+    <br>
+    <?php print'2º Teste 2' ?>
+    <br>
+
+    <?= '3º Teste' ?>
+
+    <?php 
+        // Comentario de uma linha
+        # Comentario de uma linha
+
+        /*
+        Comentario de multiplas linhas
+        */
+
+        //linha de comentario no PHP
+        //echo 'exemplos de comentarios';
+        # echo "Exemplos de comentarios";
+        echo '<br>';
+        echo 'exemplo'; 
+
+    ?>
+    
+</body>
+</html>
---
+++ Comentários PHP/Comentários.php
@@ -0,0 +1,38 @@
+<?php 
+
+echo "Exemplos de Comentarios ";
+echo "<br>";
+echo "<br>";
+
+echo "Primeiro Exemplo: ";
+echo "<br>";
+echo "<br>";
+echo " #  Comentario de uma linha";
+echo "<br>";
+echo "<br>";
+
+echo "<hr>";
+echo "<br>";
+
+echo "Segundo Exemplo: ";
+echo "<br>";
+echo "<br>";
+echo "Exemplo de comentario de multiplas linhas";
+echo "<br> /*  <br> ";
+echo "linha 1 <br>";
+echo "linha 2 ";
+echo "<br> */";
+echo "<br>";
+echo "<br>";
+echo "<hr>";
+echo "<br>";
+
+echo "Terceiro Exemplo: ";
+echo "<br>";
+echo "<br>";
+echo "// Comentario de uma linha";
+echo "<br>";
+echo "<br>";
+echo "<hr>";
+
+?>
